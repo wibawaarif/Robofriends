b1a7b1f6f627ff4eb6b07f43a257788e1c2c6290
WibawaArif <arifwb.work@gmail.com>
2022-08-11 20:43:52 +0700

    Updates

---
+++ precache-manifest.61b7b05575f0c243c7aa266a9b7a20d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f66328d5211c29096601",
+    "url": "/Robofriends/static/css/main.4510214a.chunk.css"
+  },
+  {
+    "revision": "f66328d5211c29096601",
+    "url": "/Robofriends/static/js/main.f66328d5.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "1db2e19f8c3b6323ed7b",
+    "url": "/Robofriends/static/css/2.7f63ecd8.chunk.css"
+  },
+  {
+    "revision": "1db2e19f8c3b6323ed7b",
+    "url": "/Robofriends/static/js/2.1db2e19f.chunk.js"
+  },
+  {
+    "revision": "baccc8f06f43294855f3",
+    "url": "/Robofriends/static/js/runtime~main.baccc8f0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3db70e7f2b01436845af0aa1187d7dc6",
+    "url": "/Robofriends/index.html"
+  }
+];
